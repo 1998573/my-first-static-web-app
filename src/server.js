3a6a51388f1c1a6037d1f07f7fd061b7caa0777a
1998573 <133191603+1998573@users.noreply.github.com>
2024-10-28 21:57:28 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ const authController = require('./controllers/authController');
 const recognitionController = require('./controllers/recognitionController');
 const { errorHandler } = require('./middlewares/errorHandler');
 const app = express();
+const recognitionController = require('./controllers/recognitionController');
+
+// 修改 /recognize-face 路由，添加图像上传功能
+
 
 app.use(bodyParser.json());
 
@@ -12,6 +16,7 @@ app.use(bodyParser.json());
 app.post('/register', authController.register);
 app.post('/login', authController.login);
 app.post('/recognize-face', recognitionController.recognizeFace);
+app.post('/recognize-face', recognitionController.upload, recognitionController.recognizeFace);
 
 // 错误处理
 app.use(errorHandler);
